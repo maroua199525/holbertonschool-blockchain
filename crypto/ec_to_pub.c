0334c033cbb54404a753ade63898951500ed8884
maroua199525 <2646@holbertonstudents.com>
2024-10-21 09:17:59 +0100

    add to test

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,27 +1,45 @@
 #include "hblk_crypto.h"
 
+
 /**
- * ec_to_pub - extracts the public key from an EC_KEY opaque structure
- * @key: a pointer to the EC_KEY structure
- * @pub: the address at which to store the extracted public key
- * Return: a pointer to pub, NULL on failure,
+ * ec_to_pub- func
+ * @key: EC_KEY const *
+ * @pub: uint8_t *
+ * Return: uint8_t *
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	const EC_GROUP *group;
-	const EC_POINT *point;
-	size_t len;
+	const EC_GROUP *g = NULL;
+	const EC_POINT *p = NULL;
+	point_conversion_form_t f;
 
-	if (!key || !pub)
-		return (NULL);
-
-	group = EC_KEY_get0_group(key);
-	point = EC_KEY_get0_public_key(key);
-	if (!group || !point)
-		return (NULL);
-	len = EC_POINT_point2oct(group, point,
-			POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
-	if (len != EC_PUB_LEN)
+	if (!key)
+		return (ec_to_pub_errors(1));
+	g = EC_KEY_get0_group(key);
+	if (!g)
+		return (ec_to_pub_errors(2));
+	p = EC_KEY_get0_public_key(key);
+	if (!p)
+		return (ec_to_pub_errors(3));
+	f = EC_KEY_get_conv_form(key);
+	if (EC_POINT_point2oct(g, p, f, (unsigned char *)pub, EC_PUB_LEN, NULL) == 0)
 		return (NULL);
 	return (pub);
+}
+
+
+/**
+ * ec_to_pub_errors- func
+ * @error: int
+ * Return: void *
+ */
+void *ec_to_pub_errors(int error)
+{
+	if (error == 1)
+		printf("No key passed.\n");
+	if (error == 2)
+		printf("Couldn't get group.\n");
+	if (error == 3)
+		printf("Couldn't get point.\n");
+	return (NULL);
 }
