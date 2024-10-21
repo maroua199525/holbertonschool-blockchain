7b4b2e062677fca8e86e27b9a432eff4cd7a1e05
maroua199525 <2646@holbertonstudents.com>
2024-10-21 11:18:10 +0100

    add task to test

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -1,18 +1,27 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify - Verify an ECDSA signature
- * @key: pointer to the public key to be used to verify the signature
- * @msg: points to the msglen characters to verify the signature
- * @msglen: length of the message to verify
- * @sig: pointer to the signature structure
- * Return: 1 if the signature is valid, 0 otherwise.
- */
-int ec_verify(EC_KEY const *key, uint8_t const *msg,
-				size_t msglen, sig_t const *sig)
+ * ec_verify - Verifies the signature of a given set of bytes,
+ *			   using a given EC_KEY public key.
+ * @key: Points to the EC_KEY structure containing the public key
+ *		 to be used to verify the signature.
+ * @msg: Points to the msglen characters to verify the signature of
+ * @msglen: Length of msg
+ * @sig: Points to the signature to be checked
+ * Return: 1 if the signature is valid, and 0 otherwise
+ * If either key, msg or sig is NULL, function must fail
+*/
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+			  sig_t const *sig)
 {
-	if (key == NULL || msg == NULL || sig == NULL)
+	/* Check if the key, message, or signature is NULL */
+	if (!key || !msg || !sig)
 		return (0);
-	return (ECDSA_verify(0, msg, msglen, sig->sig,
-						sig->len, (EC_KEY *)key) == 1);
+
+	/* Verify the signature */
+	if (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *) key) != 1)
+		return (0);
+
+	/* Return success if signature is valid */
+	return (1);
 }
