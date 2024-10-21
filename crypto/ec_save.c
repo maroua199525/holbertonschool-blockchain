cb0331bb5c678529d3856754be093d426b37c1dc
maroua199525 <2646@holbertonstudents.com>
2024-10-21 09:45:59 +0100

    add task to test

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,38 +1,50 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_save - Save an EC key pair on the disk
- * @key: Pointer to the EC key pair to be saved
+ * ec_save - Saves an existing EC key pair on the disk.
+ * @key: Points to the EC key pair to be saved on disk.
  * @folder: Path to the folder in which to save the keys
- *
- * Return: 1 on success, 0 on failure
+ * Return: 1 upon success, 0 on failure.
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
-	FILE *fp;
-	char path[256];
+    FILE *file_ptr;
 
-	if (!key || !folder)
-		return (0);
-	mkdir(folder, 0777);
-	sprintf(path, "%s/key.pem", folder);
-	fp = fopen(path, "w");
-	if (!fp)
-		return (0);
-	PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
-	fclose(fp);
+    if (!key || !folder)
+        return (0);
 
-	sprintf(path, "%s/key_pub.pem", folder);
-	fp = fopen(path, "w");
-	if (!fp)
-		return (0);
-	if (!PEM_write_EC_PUBKEY(fp, key))
-	{
-		fclose(fp);
-		return (0);
-	}
-	fclose(fp);
+    /* Create folder if it doesn't exist */
+    if (mkdir(folder, 0777) == -1)
+    {
+        if (errno != EEXIST)
+            return (0);
+    }
 
-	return (1);
+    /* Change process directory to folder */
+    if (chdir(folder) != 0)
+        return (0);
 
+    /* Write the private key to private PEM file */
+    file_ptr = fopen(PRI_FILENAME, "w");
+    if (!file_ptr)
+        return (0);
+    if (PEM_write_ECPrivateKey(file_ptr, key, NULL, NULL, 0, NULL, NULL) == 0)
+    {
+        fclose(file_ptr);
+        return (0);
+    }
+    fclose(file_ptr);
+
+    /* Write the public key to public PEM file */
+    file_ptr = fopen(PUB_FILENAME, "w");
+    if (!file_ptr)
+        return (0);
+    if (PEM_write_EC_PUBKEY(file_ptr, key) == 0)
+    {
+        fclose(file_ptr);
+        return (0);
+    }
+    fclose(file_ptr);
+
+    return (1);
 }
