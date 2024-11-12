3cafb309e86442d395dce33cec7afc98ba44fefb
maroua199525 <2646@holbertonstudents.com>
2024-11-12 13:58:14 +0100

    add to test

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,41 +1,46 @@
 #include "hblk_crypto.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * ec_load - Load an EC key pair from disk
- * @folder: Path to the folder from which to load the keys
- * Return: Pointer to the EC key pair upon success, or NULL upon failure
+ * ec_load - loads an EC key pair from the given folder
+ * @folder: path to the folder containing key.pem (private key) and key_pub.pem (public key)
+ * Return: EC_KEY* on success, or NULL on failure
  */
 EC_KEY *ec_load(char const *folder)
 {
-    FILE *fp = NULL;
-    char key_path[256] = {0};
+    FILE *fd = NULL;
     EC_KEY *key = NULL;
 
-    if (!folder)
-        return (NULL);
-
-    /* Construct path to public key file */
-
-    sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
-
-    /* Open and read public key */
-    fp = fopen(key_path, "r");
-    if (!fp || !PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-        return (NULL);
-    fclose(fp);
-
-    /* Construct path to private key file */ 
-    sprintf(key_path, "%s/%s", folder, PRI_FILENAME);
-
-    /* Open and read private key */ 
-    fp = fopen(key_path, "r");
-    if (!fp || !PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-    {
-        EC_KEY_free(key); /* free because allocated for public already */
-		return (NULL);
+    // Check if the folder path is valid and change the directory
+    if (!folder || chdir(folder) == -1) {
+        perror("chdir failed");
+        return NULL;
     }
-    fclose(fp);
-    return (key);
+
+    // Create a new EC key object with the specified curve
+    key = EC_KEY_new_by_curve_name(EC_CURVE);
+    if (!key) {
+        perror("EC_KEY_new_by_curve_name failed");
+        return NULL;
+    }
+
+    // Open the public key file (key_pub.pem) and read the key
+    fd = fopen("key_pub.pem", "r");
+    if (!fd || !PEM_read_EC_PUBKEY(fd, &key, NULL, NULL)) {
+        perror("fopen or PEM_read_EC_PUBKEY failed");
+        EC_KEY_free(key);
+        return NULL;
+    }
+    fclose(fd);
+
+    // Open the private key file (key.pem) and read the key
+    fd = fopen("key.pem", "r");
+    if (!fd || !PEM_read_ECPrivateKey(fd, &key, NULL, NULL)) {
+        perror("fopen or PEM_read_ECPrivateKey failed");
+        EC_KEY_free(key);
+        return NULL;
+    }
+    fclose(fd);
+
+    // Return the loaded EC key pair
+    return key;
 }
