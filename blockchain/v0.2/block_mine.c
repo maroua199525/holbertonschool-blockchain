0374913712423f66d2ebcaa16b3189344375e062
maroua199525 <2646@holbertonstudents.com>
2024-12-05 10:22:04 +0100

    add task

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,15 +1,17 @@
 #include "blockchain.h"
 
+
 /**
- * block_mine - mines a block
- * @block: pointer to the block to be mined
- * Return: nothing (void)
+ * block_mine- func
+ * @block: block_t *
+ * Return: void
  */
 void block_mine(block_t *block)
 {
-	block->info.nonce = 0;
-	do {
-		block->info.nonce += 1;
-		block_hash(block, block->hash);
-	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+	if (block)
+		while (!hash_matches_difficulty(block->hash, block->info.difficulty))
+		{
+			block->info.nonce++;
+			block_hash(block, block->hash);
+		}
 }
