91b727cbdf3053eb2bb5ca585403ef597ff76f36
maroua199525 <2646@holbertonstudents.com>
2024-12-05 10:24:35 +0100

    add task

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,19 +1,18 @@
 #include "blockchain.h"
 
-
 /**
- * block_mine- func
- * @block: block_t *
- * Return: void
+ * block_mine - Mines a Block in order to insert it in the Blockchain
+ * @block: Pointer to the Block to be mined
+ *
+ * The function finds a hash for the block that matches its difficulty.
  */
 void block_mine(block_t *block)
 {
-	if (!block)
+    if (!block)
         return;
-	if (block)
-		while (!hash_matches_difficulty(block->hash, block->info.difficulty))
-		{
-			block->info.nonce++;
-			block_hash(block, block->hash);
-		}
+
+    do {
+        block_hash(block, block->hash);
+    } while (!hash_matches_difficulty(block->hash, block->info.difficulty) &&
+             ++block->info.nonce);
 }
