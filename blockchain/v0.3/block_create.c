d76bad5f810039a85625270de0662384913398c6
maroua199525 <2646@holbertonstudents.com>
2024-12-23 15:07:26 +0100

    add to test

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -8,43 +8,26 @@
  * @data_len: uint32_t
  * Return: block_t *
  */
-
-block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len)
 {
-	block_t *new_block = malloc(sizeof(block_t));
+	block_t *nb = NULL;
+	uint32_t dl = 0;
 
-	if (!new_block)
+	if (!prev || !data)
 		return (NULL);
-
-	/* Initialize struct members to zero */
-	memset(new_block, 0, sizeof(block_t));
-
-	/* Block info initialisation*/
-	if (prev)
-	{
-		new_block->info.index = prev->info.index + 1;
-		memcpy(new_block->info.prev_hash, prev->hash,
-			   sizeof(new_block->info.prev_hash));
-	}
-	new_block->info.timestamp = time(NULL);
-	/* new_block->info.timestamp = 1537207477; */ /* hard code for tests */
-
-	/* Block data initialisation */
-	if (data && data_len)
-	{
-		if (data_len > BLOCKCHAIN_DATA_MAX)
-			data_len = BLOCKCHAIN_DATA_MAX;
-		memcpy(new_block->data.buffer, data, data_len);
-		new_block->data.len = data_len;
-	}
-
-	/* transaction empty list creation */
-	new_block->transactions = llist_create(MT_SUPPORT_FALSE);
-	if (!new_block->transactions)
-	{
-		free(new_block);
+	nb = malloc(sizeof(block_t));
+	if (!nb)
 		return (NULL);
-	}
-
-	return (new_block);
+	nb->info.index = prev->info.index + 1;
+	nb->info.difficulty = 0;
+	nb->info.timestamp = time(NULL);
+	nb->info.nonce = 0;
+	memcpy(nb->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	dl = (data_len > BLOCKCHAIN_DATA_MAX) ? BLOCKCHAIN_DATA_MAX : data_len;
+	memcpy(nb->data.buffer, data, dl);
+	nb->data.len = dl;
+	nb->transactions = llist_create(MT_SUPPORT_FALSE);
+	memset(nb->hash, 0, SHA256_DIGEST_LENGTH);
+	return (nb);
 }
--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -9,7 +9,7 @@
 void block_destroy(block_t *block)
 {
 	if (!block)
-		llist_destroy(block->transactions, 1,
-		(node_dtor_t)transaction_destroy);
+		return;
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
 	free(block);
 }
