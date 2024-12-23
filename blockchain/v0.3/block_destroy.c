13d38ce5f064fbffeae4610604bfda7f88f369dc
maroua199525 <2646@holbertonstudents.com>
2024-12-23 15:16:38 +0100

    add to test

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -8,7 +8,7 @@
  */
 void block_destroy(block_t *block)
 {
-	if (!block->transactions)
+	if (!block)
 	return;
 	llist_destroy(block->transactions, 1,(node_dtor_t)transaction_destroy);
 	free(block);
