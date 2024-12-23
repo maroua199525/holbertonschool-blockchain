72fb5e7164c418a2e03bb0a744297ca6afa245df
maroua199525 <2646@holbertonstudents.com>
2024-12-23 15:04:29 +0100

    add task

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -8,39 +8,43 @@
  * @data_len: uint32_t
  * Return: block_t *
  */
-block_t *block_create(block_t const *prev, int8_t const *data,
-	uint32_t data_len)
+
+block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-	block_t *nb = NULL;
-	nb = malloc(sizeof(block_t));
-	if (!nb)
+	block_t *new_block = malloc(sizeof(block_t));
+
+	if (!new_block)
 		return (NULL);
 
-	
 	/* Initialize struct members to zero */
-	memset(nb, 0, sizeof(block_t));
+	memset(new_block, 0, sizeof(block_t));
 
+	/* Block info initialisation*/
 	if (prev)
 	{
-		nb->info.index = prev->info.index + 1;
-		memcpy(nb->info.prev_hash, prev->hash,
-			   sizeof(nb->info.prev_hash));
+		new_block->info.index = prev->info.index + 1;
+		memcpy(new_block->info.prev_hash, prev->hash,
+			   sizeof(new_block->info.prev_hash));
 	}
-	nb->info.timestamp = time(NULL);
+	new_block->info.timestamp = time(NULL);
+	/* new_block->info.timestamp = 1537207477; */ /* hard code for tests */
+
 	/* Block data initialisation */
 	if (data && data_len)
 	{
 		if (data_len > BLOCKCHAIN_DATA_MAX)
 			data_len = BLOCKCHAIN_DATA_MAX;
-		memcpy(nb->data.buffer, data, data_len);
-		nb->data.len = data_len;
+		memcpy(new_block->data.buffer, data, data_len);
+		new_block->data.len = data_len;
 	}
 
-	nb->transactions = llist_create(MT_SUPPORT_FALSE);
-	if (!nb->transactions)
+	/* transaction empty list creation */
+	new_block->transactions = llist_create(MT_SUPPORT_FALSE);
+	if (!new_block->transactions)
 	{
-		free(nb);
+		free(new_block);
 		return (NULL);
 	}
-	return (nb);
+
+	return (new_block);
 }
