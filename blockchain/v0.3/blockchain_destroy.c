3a48b98b602be4102fd1057bc81a6b656598501d
maroua199525 <2646@holbertonstudents.com>
2024-12-23 15:35:39 +0100

    add to test

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -8,9 +8,9 @@
  */
 void blockchain_destroy(blockchain_t *blockchain)
 {
-	if (blockchain->chain)
-		llist_destroy(blockchain->chain, 1, (node_dtor_t)block_destroy);
-	if (blockchain->unspent)
-		llist_destroy(blockchain->unspent, 1, NULL);
+	if (!blockchain)
+		return;
+	llist_destroy(blockchain->chain, 1, (node_dtor_t)block_destroy);
+	llist_destroy(blockchain->unspent, 1, NULL);
 	free(blockchain);
 }
