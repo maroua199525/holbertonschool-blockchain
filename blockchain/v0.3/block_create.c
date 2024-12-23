9c5b3aeafa4d1405ca5a3c8adcabe78403abd8bd
maroua199525 <2646@holbertonstudents.com>
2024-12-23 15:02:09 +0100

    add task

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -8,44 +8,40 @@
  * @data_len: uint32_t
  * Return: block_t *
  */
-block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len)
 {
-    block_t *nb = malloc(sizeof(block_t));
-    uint32_t dl = 0;
+	block_t *nb = NULL;
+	uint32_t dl = 0;
+	nb = malloc(sizeof(block_t));
+	if (!nb)
+		return (NULL);
 
-    if (!nb)
-        return (NULL);
+	
+	/* Initialize struct members to zero */
+	memset(nb, 0, sizeof(block_t));
 
-    /* Initialize all fields to 0 */
-    memset(nb, 0, sizeof(block_t));
+	if (prev)
+	{
+		nb->info.index = prev->info.index + 1;
+		memcpy(nb->info.prev_hash, prev->hash,
+			   sizeof(nb->info.prev_hash));
+	}
+	nb->info.timestamp = time(NULL);
+	/* Block data initialisation */
+	if (data && data_len)
+	{
+		if (data_len > BLOCKCHAIN_DATA_MAX)
+			data_len = BLOCKCHAIN_DATA_MAX;
+		memcpy(nb->data.buffer, data, data_len);
+		nb->data.len = data_len;
+	}
 
-    /* Set block index and previous hash if prev is not NULL */
-    if (prev)
-    {
-        nb->info.index = prev->info.index + 1;
-        memcpy(nb->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-    }
-
-    nb->info.timestamp = time(NULL);
-
-    /* Limit data length and copy */
-    if (data && data_len > 0)
-    {
-        dl = (data_len > BLOCKCHAIN_DATA_MAX) ? BLOCKCHAIN_DATA_MAX : data_len;
-        memcpy(nb->data.buffer, data, dl);
-        nb->data.len = dl;
-    }
-
-    /* Create an empty transaction list */
-    nb->transactions = llist_create(MT_SUPPORT_FALSE);
-    if (!nb->transactions)
-    {
-        free(nb);
-        return (NULL);
-    }
-
-    /* Zero the hash field */
-    memset(nb->hash, 0, SHA256_DIGEST_LENGTH);
-
-    return (nb);
+	nb->transactions = llist_create(MT_SUPPORT_FALSE);
+	if (!nb->transactions)
+	{
+		free(nb);
+		return (NULL);
+	}
+	return (nb);
 }
