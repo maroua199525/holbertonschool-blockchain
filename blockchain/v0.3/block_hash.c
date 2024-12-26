5f023499dab2bb0747b4b9e2d77e5c508a6ade68
maroua199525 <2646@holbertonstudents.com>
2024-12-26 15:55:32 +0100

    add to test

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -23,11 +23,8 @@ uint8_t *block_hash(block_t const *block,
 		return (NULL);
 	current_pos = buf;
 	memcpy(current_pos, block, sizeof(block->info) + block->data.len);
-	if (tx_size > 0)
-	{
-		current_pos += sizeof(block->info) + block->data.len;
-		llist_for_each(block->transactions, (node_func_t)cpy_tx, current_pos);
-	}
+	current_pos += sizeof(block->info) + block->data.len;
+	llist_for_each(block->transactions, (node_func_t)cpy_tx, current_pos);
 	sha256((int8_t *)buf, b_length, hash_buf);
 	free(buf);
 	return (hash_buf);
